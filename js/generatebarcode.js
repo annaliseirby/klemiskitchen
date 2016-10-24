77b6c3db40cc846b1c0995c9d4f5d229995c8937
Erica Rabinovich <ericar13@live.com>
2016-10-24 16:59:13 -0400

    Updated barcode PDF

--- js/generatebarcode.js
+++ js/generatebarcode.js
@@ -124,25 +124,25 @@ $(function() {
         var doc = { content: [
             { table: {
                 body: [
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
-                    [ { image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}, {image: 'barcode', width: 173, height: 75}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
+                    [ { image: 'barcode', width: 155, height: 70}, {image: 'barcode', width: 190, height: 70}, {image: 'barcode', width: 155, height: 70}],
                 ]
             },
                 layout: {
                     hLineWidth: function(i, node) { return 0; },
                     vLineWidth: function(i, node) { return 0; },
-                    paddingLeft: function(i, node) { return 0; },
-                    paddingRight: function(i, node) { return 0; },
+                    paddingLeft: function(i, node) { return 3.5; },
+                    paddingRight: function(i, node) { return 3.5; },
                     paddingTop: function(i, node) { return 0; },
-                    paddingBottom: function(i, node) { return 0; }
+                    paddingBottom: function(i, node) { return 2; }
                 }
             },
         ],
