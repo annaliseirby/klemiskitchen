34228e50d308d644c47dd1e624ec0b22ad293cca
kevin-tian <kevin_tian@outlook.com>
2016-11-03 19:11:32 -0400

    Added outline for barcode scanning

--- js/scanBarcode.js
+++ js/scanBarcode.js
@@ -2,32 +2,49 @@
  * Created by kevin on 10/13/2016.
  */
 $(document).ready(function () {
+
+
+    $('input').keypress(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            $("#inputBarcodes").submit();
+        }
+    });
+
+
+
     $('#inputBarcodes').on('submit', function(e) {
         e.preventDefault();
+        var barcode = $("#barcodeText").val();
+        $('#currentItems').append('<li class="list-group-item">' + barcode + '</li>');
 
-        var barcodes = $("#barcodeText").val().split('\n'); //gets input as an array of each line
-        for (var i = 0; i < barcodes.length; i++) { //loops through each line
-            var date = barcodes[i].substr(0, 5); //isolates the date
-            var data = barcodes[i].substr(6);
-            var item = {date: date, data: data};
-            barcodes[i] = item;
-        }
 
-        console.log(barcodes);
 
+        // var barcodes = $("#barcodeText").val().split('\n'); //gets input as an array of each line
+        // for (var i = 0; i < barcodes.length; i++) { //loops through each line
+        //     var date = barcodes[i].substr(0, 5); //isolates the date
+        //     var data = barcodes[i].substr(6);
+        //     var item = {date: date, data: data};
+        //     barcodes[i] = item;
+        // }
+
+        //console.log(barcodes);
+
+        //Clears contents of the form
         document.getElementById("inputBarcodes").reset();
-        $.ajax({
-            type: 'POST',
-            url: '../php/barcode.php',
-            data: barcodes,
-            success : function(data) {
-                console.log(data);
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                alert('An error has occured! Please try resubmitting.');
-                console.log(errorThrown);
-            }
-        });
+
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '../php/barcode.php',
+        //     data: barcodes,
+        //     success : function(data) {
+        //         console.log(data);
+        //     },
+        //     error: function(XMLHttpRequest, textStatus, errorThrown) {
+        //         alert('An error has occured! Please try resubmitting.');
+        //         console.log(errorThrown);
+        //     }
+        // });
 
     });
 });
