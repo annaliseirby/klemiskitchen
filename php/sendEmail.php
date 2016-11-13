e4996325ec29e0241d13da956c2769d3a6695d5e
kevin-tian <kevin_tian@outlook.com>
2016-11-13 13:33:10 -0500

    Changed organization for deployment, fixed navigation issues on admin pages

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -5,5 +5,12 @@
  * Date: 11/11/2016
  * Time: 5:41 PM
  */
-echo("Email: " . $_POST["email"] . "\n");
-echo("Message: " . $_POST["message"]);
+$from = $_POST["email"];
+$subject = "New Klemis Kitchen Application";
+$msg = $_POST["message"];
+$msg = wordwrap($msg,70);
+$headers = "From: " . $from;
+
+echo ($from + "\n" + $subject + "\n" + $msg + "\n" + $headers);
+//mail("rtech153@gmail.com", $subject, $msg, $headers);
+
