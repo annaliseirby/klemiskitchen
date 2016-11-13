76de5efb1a13635f218e4950703316de086ddadc
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-11-13 13:31:02 -0500

    Parser and INSERT query added to barcode mysqli_connect updated

--- php/barcode.php
+++ php/barcode.php
@@ -1,30 +1,52 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: kevin
- * Date: 10/13/2016
- * Time: 5:03 PM
- */
-    echo "TESTER";
-//    <A HREF = "../homepage.php">Home Page</A> </br>
-//
-//    <form action="barcode.php" method = "post">
-//
-//        require_once('../php/mysqli_connect.php');
-//        $query= "SELECT * FROM MEAL";
-//        $response= mysqli_query($dbc, $query);
-//        if($response){
-//            while($row = mysqli_fetch_array($response)) {
-//                echo "<option value=\"$row[Date]\"</option>";
-//                echo '<tr><td align = "left">' .
-//                $row['Date'] . '</td><td align = "left">';
-//                echo '</tr>';
-//            }
-//        } else {
-//             echo "Couldn't perform query";
-//             echo mysqli_error($dbc);
-//        }
+<html>
+    <?php
+    /**
+    * Created by IntelliJ IDEA.
+    * User: kevin
+    * Date: 10/13/2016
+    * Time: 5:03 PM
+    */
+    //    <A HREF = "../homepage.php">Home Page</A> </br>
+    //
+    //    <form action="barcode.php" method = "post">
+    //
+    //        require_once('../php/mysqli_connect.php');
+    //        $query= "SELECT * FROM MEAL";
+    //        $response= mysqli_query($dbc, $query);
+    //        if($response){
+    //            while($row = mysqli_fetch_array($response)) {
+    //                echo "<option value=\"$row[Date]\"</option>";
+    //                echo '<tr><td align = "left">' .
+    //                $row['Date'] . '</td><td align = "left">';
+    //                echo '</tr>';
+    //            }
+    //        } else {
+    //             echo "Couldn't perform query";
+    //             echo mysqli_error($dbc);
+    //        }
 
+            $data = 1011011101110;
+            $binStr = strval($data);
 
+            $poultry = substr($binStr, 0, 1);
+            $pork = substr($binStr, 1, 1);
+            $fish = substr($binStr, 2, 1);
+            $beef = substr($binStr, 3, 1);
+            $bread = substr($binStr, 4, 1);
+            $pasta = substr($binStr, 5, 1);
+            $rice = substr($binStr, 6, 1);
+            $potato = substr($binStr, 7, 1);
+            $vegetable = substr($binStr, 8, 1);
+            $fruit = substr($binStr, 9, 1);
+            $sandwhich = substr($binStr, 10, 1);
+            $simplytogo = substr($binStr, 11, 1);
+            $dessert = substr($binStr, 12, 1);
+            $date = date('Y-m-d');
 
-?>
+            echo("$date$poultry$pork$fish$beef$bread$pasta$rice$potato$vegetable$fruit$sandwhich$simplytogo$dessert");
+
+            //INSERT INTO MEAL VALUES ($date, $poultry, $pork, $fish, $beef, $bread, $pasta, $rice, $potato, $vegetable,
+            //$fruit, $sandwhich, $simplytogo, $dessert);
+
+    ?>
+</html>
--- php/mysqli_connect.php
+++ php/mysqli_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE ('DB_USER', 'user');
-DEFINE ('DB_PASSWORD', 'pass');
+DEFINE ('DB_USER', 'drux_s2klemib');
+DEFINE ('DB_PASSWORD', 'IasDPVoQGWBTWRO8');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'MEALDATABASE');
+DEFINE ('DB_NAME', 'drux_s2klemi');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL' .
