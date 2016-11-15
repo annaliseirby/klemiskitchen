3ba4d5581686bcd4f83b8bd4bc8d3fbab2a1d478
kevin-tian <kevin_tian@outlook.com>
2016-11-14 19:51:19 -0500

    Added code for datatables

--- js/inventoryTable.js
+++ js/inventoryTable.js
@@ -6,11 +6,36 @@ $(function() {
         type: 'GET',
         url: '../../php/fullInventory.php',
         success : function(data) {
-            console.log(data);
+            drawTable(data);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert('An error has occured! Please try resubmitting.');
             console.log(errorThrown);
         }
     });
+
+    function drawTable(data) {
+        $('#inventoryTable').DataTable( {
+            pageLength : 25,
+            data: data,
+            columns: [
+                { data: "Date" },
+                { data: "Pork" },
+                { data: "Poultry" },
+                { data: "Fish" },
+                { data: "Beef" },
+                { data: "Bread" },
+                { data: "Pasta:" },
+                { data: "Rice" },
+                { data: "Potato" },
+                { data: "Vegetable" },
+                { data: "Fruit" },
+                { data: "Sandwhich" },
+                { data: "SimplyToGo" },
+                { data: "Dessert" },
+                { data: "NumMeals" }
+            ]
+        });
+    }
+
 });
--- php/fullInventory.php
+++ php/fullInventory.php
@@ -5,20 +5,24 @@
  * Date: 11/14/2016
  * Time: 7:02 PM
  */
-    require_once('../php/mysqli_connect.php');
-    $dbc= new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
-    $query = "SELECT * FROM MEAL";
-    $response = mysqli_query($dbc, $query);
-    if($response) {
-        $encode = array();
-        while($row = mysqli_fetch_assoc($response)) {
-            $encode[] = $row;
-        }
-        echo json_encode($encode);
-    } else {
-        echo "Couldn't perform query";
-        echo mysqli_error($dbc);
+$DB_USER = 'klemiss2';
+$DB_PASSWORD = 'klemis2017';
+$DB_HOST = 'localhost';
+$DB_NAME = 'drux_s2klemi';
+$dbc = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+
+$query = "SELECT * FROM MEAL";
+$response = mysqli_query($dbc, $query);
+if ($response) {
+    $encode = array();
+    while ($row = mysqli_fetch_assoc($response)) {
+        $encode[] = $row;
     }
-    mysqli_close($dbc);
+    echo json_encode($encode);
+} else {
+    echo "Couldn't perform query";
+    echo mysqli_error($dbc);
+}
+mysqli_close($dbc);
 
 ?>
