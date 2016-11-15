c77a099c7d632b1cedaf71cb347e077cfca456fa
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-11-14 19:53:13 -0500

    Updated barcode.php to deal with already existing rows

--- php/barcode.php
+++ php/barcode.php
@@ -7,6 +7,7 @@
  */
 
 $data = $_POST['data'];
+
 $binStr = strval($data);
 
 $poultry = substr($binStr, 0, 1);
@@ -28,14 +29,29 @@ $DB_PASSWORD = 'klemis2017';
 $DB_HOST = 'localhost';
 $DB_NAME = 'drux_s2klemi';
 $dbc = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
-
-
-$sql = "INSERT INTO MEAL VALUES(now(), $poultry, $pork, $fish, $beef, $bread, $pasta,
-              $rice, $potato, $vegetable, $fruit, $sandwhich, $simplytogo, $dessert, 1)";
-if ($dbc->query($sql) === TRUE) {
-    echo "New record created successfully";
+$sql1 = "SELECT EXISTS(SELECT * FROM MEAL WITH VALUE WHERE Date = now() AND Poultry = $poultry AND Pork = $pork AND Fish = $fish
+  AND Beef = $beef AND Pasta = $pasta AND Rice = $rice AND Potato = $Potato AND Vegetable = $vegetable
+  AND Fruit = $fruit AND Sandwhich $sandwhich AND SimplyToGo = $simplytogo AND Dessert = $Dessert)";
+if(mysqli_result($sql1, 0) == 1) {
+    $sql2 = "UPDATE MEAL SET NumMeals = (SELECT NumMeals FROM MEAL WITH VALUE WHERE Date = now() AND Poultry = $poultry AND Pork = $pork AND Fish = $fish
+          AND Beef = $beef AND Pasta = $pasta AND Rice = $rice AND Potato = $Potato AND Vegetable = $vegetable
+          AND Fruit = $fruit AND Sandwhich $sandwhich AND SimplyToGo = $simplytogo AND Dessert = $Dessert) + 1
+          WHERE Date = now() AND Poultry = $poultry AND Pork = $pork AND Fish = $fish
+          AND Beef = $beef AND Pasta = $pasta AND Rice = $rice AND Potato = $Potato AND Vegetable = $vegetable
+          AND Fruit = $fruit AND Sandwhich $sandwhich AND SimplyToGo = $simplytogo AND Dessert = $Dessert";
+        if ($conn->query($sql2) === TRUE) {
+                echo "Record updated successfully";
+        } else {
+                echo "Error updating record: " . $conn->error;
+        }
 } else {
-    echo "Error: " . $sql . "<br>" . $dbc->error;
+        $sql = "INSERT INTO MEAL VALUES(now(), $poultry, $pork, $fish, $beef, $bread, $pasta,
+              $rice, $potato, $vegetable, $fruit, $sandwhich, $simplytogo, $dessert, 1)";
+        if ($dbc->query($sql) === TRUE) {
+                echo "New record created successfully";
+        } else {
+                echo "Error: " . $sql . "<br>" . $dbc->error;
+        }
 }
 $dbc->close();
 ?>
