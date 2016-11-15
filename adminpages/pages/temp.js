9b7e64a225b9cc6b084cf45acbcae42733f3bad6
annaliseirby <annalise.irby@gmail.com>
2016-11-15 00:01:01 -0500

    Adding html files

--- adminpages/pages/temp.js
+++ adminpages/pages/temp.js
@@ -1,11 +1,40 @@
 
-var width = 960,
+var width = document.getElementById("morris-area-chart").offsetWidth,
     height = 500,
     radius = Math.min(width, height) / 2 - 10;
 
-var data = d3.range(5).map(Math.random).sort(d3.descending);
+var myArr = d3.range(6).map(Math.random).sort(d3.descending);
+
+var data = d3.range(6).map(Math.random).sort(d3.descending);
+
+var labels = ['Poultry', 'Pork', 'Beef', 'Multi-Meat', 'Veggie', 'Fish'];
+
+
+//
+// var sliceLabel = label_group.selectAll("text")
+//     .data(donut(data.pct));
+// sliceLabel.enter().append("svg:text")
+//     .attr("class", "arcLabel")
+//     .attr("transform", function(d) {return "translate(" + arc.centroid(d) + ")"; })
+//     .attr("text-anchor", "middle")
+//     .text(function(d, i) {return labels[i]; });
 
 var color = d3.scale.category20();
+
+for (var i = 0; i < labels.length; i++) {
+    try {
+        console.log("CHANGING" + labels[i].toLowerCase());
+        console.log("BACKGROUND COLOR: ");
+        console.log(color(i));
+        document.getElementById(labels[i].toLowerCase()).style.backgroundColor = color(i);
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+console.log("Color is ");
+console.log(color);
 
 var arc = d3.svg.arc()
     .outerRadius(radius);
@@ -36,6 +65,24 @@ arcs.append("path")
     .duration(750)
     .attrTween("d", tweenDonut);
 
+// arcs.append("svg:text")
+//     .attr("transform", function(d) {
+//         var c = arc.centroid(d),
+//             x = c[0],
+//             y = c[1],
+//             // pythagorean theorem for hypotenuse
+//             h = Math.sqrt(x*x + y*y);
+//         return "translate(" + (x/h * labelr) +  ',' +
+//             (y/h * labelr) +  ")";
+//     })
+//     .attr("dy", ".35em")
+//     .attr("text-anchor", function(d) {
+//         // are we past the center?
+//         return (d.endAngle + d.startAngle)/2 > Math.PI ?
+//             "end" : "start";
+//     })
+//     .text(function(d, i) { return d.value.toFixed(2); });
+
 function tweenPie(b) {
     b.innerRadius = 0;
     var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
@@ -47,3 +94,4 @@ function tweenDonut(b) {
     var i = d3.interpolate({innerRadius: 0}, b);
     return function(t) { return arc(i(t)); };
 }
+
